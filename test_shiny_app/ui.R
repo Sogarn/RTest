3a52007d55ce366262e38249c89e13c0a4ddce7b
Sogarn <sogarnsystems@gmail.com>
2026-04-27 18:46:13 -0400

    Test widget UI

--- test_shiny_app/ui.R
+++ test_shiny_app/ui.R
@@ -3,17 +3,90 @@ library(bslib)
 
 # Define UI ----
 fluidPage(
-  # Set up sidebar
-  page_sidebar(
-    title = "My Shiny App",
-    sidebar = sidebar(global_sidebar_text,
-                      code('install.packages("shiny")')
-                      ),
+  # Set up widgets
+  titlePanel("Basic widgets"),
+  layout_columns(
+    col_width = 3,
     card(
-      card_header("Introducing Shiny"),
-      global_card_text,
-      card_image("https://shiny.posit.co/r/getstarted/shiny-basics/lesson2/images/my-shiny-app.png", height = "300px"),
-      card_footer("Shiny is a product of Posit")
+      card_header("Buttons"),
+      actionButton("action", "Action"),
+      submitButton("Submit")
+    ),
+    card(
+      card_header("Single checkbox"),
+      checkboxInput("checkbox", "Choice A", value = TRUE)
+    ),
+    card(
+      card_header("Checkbox group"),
+      checkboxGroupInput(
+        "checkGroup",
+        "Select all that apply",
+        choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+        selected = 1
+      )
+    ),
+    card(
+      card_header("Date input"),
+      dateInput("date", "select date", value = "2014-01-01")
+    ),
+    card(
+      card_header("Date range input"),
+      dateRangeInput("dates", "Select dates")
+    ),
+    card(
+      card_header("File input"),
+      fileInput("file", label = NULL)
+    ),
+    card(
+      card_header("Help text"),
+      helpText(
+        "Note: help text isn't a true widget,",
+        "but it provides an easy way to add texxt to",
+        "accompany other widgets."
+      )
+    ),
+    card(
+      card_header("Numeric input"),
+      numericInput("num", "Input number", value = 1)
+    ),
+    card(
+      card_header("Radio buttons"),
+      radioButtons(
+        "radio",
+        "Select option",
+        choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+        selected = 1
+      )
+    ),
+    card(
+      card_header("Select box"),
+      selectInput(
+        "select",
+        "Select option",
+        choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+        selected = 1
+      )
+    ),
+    card(
+      card_header("Sliders"),
+      sliderInput(
+        "slider1",
+        "Set value",
+        min = 0,
+        max = 100,
+        value = 50
+      ),
+      sliderInput(
+        "slider2",
+        "Set value",
+        min = 0,
+        max = 100,
+        value = c(25, 75)
+      )
+    ),
+    card(
+      card_header("Text input"),
+      textInput("text", label = NULL, value = "Enter text...")
     )
   )
 )
